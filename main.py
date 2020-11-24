abd2dcfc5ddb71388127d2bf45159030ffe469c2
shermanflan <shermanflan@gmail.com>
2020-11-23 20:40:52 -0600

    Working Kubernetes Executor

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+def countBits(num):
+    result, i = [0], 1
+
+    while i <= num:
+
+        if (num - 1) & 1 == 0:  # even
+            result.append(result[i - 1] + 1)
+        else:
+            if i & (i - 1) == 0:  # power of 2
+                result.append(1)
+            else:
+                result.append(result[i - 1])
+        i += 1
+
+    return result
+
 
 if __name__ == '__main__':
 
@@ -23,3 +39,5 @@ if __name__ == '__main__':
     # r = redis.Redis(host='40.76.155.221', port=6379, db=0)
     # r.set('rko', '21')
     # logger.info(f"RESULT: {r.get('foo')}")
+
+    countBits(5)
